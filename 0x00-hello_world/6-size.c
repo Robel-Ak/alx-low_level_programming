a672bd4017e8531e2d9e9de406de4f2eaabb88c7
Robel amde <Robelamde21@gmail.com>
2022-03-14 15:09:23 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,8 @@ int main(void)
 	long long int c;
 	char d;
 	float f;
+
+
 	printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
