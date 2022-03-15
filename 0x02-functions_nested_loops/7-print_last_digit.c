1f299141f9a030e165968a5c653217b9ff2eca39
Robel amde <Robelamde21@gmail.com>
2022-03-15 11:39:05 -0700

    Task -7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,8 +6,10 @@
  */
 int print_last_digit(int n)
 {
+	int l = n % 10;
+
 	if (n < 0)
-		n = -1 * n;	
-	_putchar('0' + ( n % 10));
-	return (n % 10);
+		n = -1 * n;
+	_putchar('0' + l);
+	return (l);
 }
