6215559ecbf7588f0db98bd7c540c678d27f62a4
Robel amde <Robelamde21@gmail.com>
2022-03-15 11:19:31 -0700

    Task -7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit num
+ * @n: to find the last place of the number
+ * Return: the last digit
+ */
+int print_last_digit(int n)
+{
+	if (n < 0)
+		n = n * -1;
+	_putchar((n % 10) + '0');
+	return (n % 10);
+}
