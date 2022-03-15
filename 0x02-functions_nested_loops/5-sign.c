a753cd1d7f19da7fe313c16bce42737f32f55dcf
Robel amde <Robelamde21@gmail.com>
2022-03-15 11:12:39 -0700

    Task -5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_sign - Determins the sign of integer.
- * @n: the number 
+ * @n: the number
  * Return: 1 if positive, 0 if zero, -1 if negative
  */
 int print_sign(int n)
