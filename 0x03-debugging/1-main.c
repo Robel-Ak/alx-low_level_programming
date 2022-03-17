c851cd829fcef92671f3548036c4eef3899c8dd0
Robel Amde <Robelamde21@gmail.com>
2022-03-17 11:37:33 -0700

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
  * main - causes an infinte loop
- *
  * Return: 0
  */
 
@@ -13,7 +12,8 @@ int main(void)
 
 	i = 0;
 
-	 /* while (i < 10)
+	 /*
+	 * while (i < 10)
 	 * {
 	 * 	putchar(i);
 	 * }
