341706052c78da619f9e8471891f351e183a6ccd
Robel Amde <Robelamde21@gmail.com>
2022-03-21 13:17:10 -0700

    Task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts_half - prints the second
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int length, n ,i;
+
+	for (length = 0; str[length] != '\0'; length++)
+	{
+	}
+	n = (length - 1) / 2;
+       for ( i = n + !; str[i] != '\0'; i++)
+       {
+	       _putchar(str[i]);
+       }
+       _putchar('\n');
+}       
