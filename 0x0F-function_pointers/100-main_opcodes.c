ef7c4419796a9549c24b74444cc8b16a0a14e024
Robel-Ak <Robelamde21@gmail.com>
2022-04-13 17:04:50 +0300

    Function_pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,45 +1,39 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <udis86.h>
 
 /**
- * print_opcodes - Print the opcodes of this program.
- * @s: Address of the main function.
- * @x: Number of bytes to print.
- **/
-void print_opcodes(char *s, int x)
+  * main - ...
+  * @argc: ...
+  * @argv: ...
+  *
+  * Return: ...
+  */
+int main(int argc, char *argv[])
 {
-	int i = 0;
+	ud_t ud_obj;
+	int val = 0, i = 0;
 
-	for (i = 0; i < x; i++)
+	if (argc == 2)
 	{
-		printf("%.2hhx", s[i]);
-		if (i < x - 1)
-			printf(" ");
-	}
-	printf("\n");
-}
+		val = atoi(argv[1]);
 
-/**
- * main - Print the opcodes of this main function.
- * @argc: Number of arguments.
- * @argv: Arguments vector.
- * Return: O (success).
- */
-int main(int argc, char **argv)
-{
-	int x;
+		if (val < 0)
+		{
+			printf("Error\n");
+			exit(2);
+		}
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
+		ud_unit(&ud_obj);
+		ud_set_input_buffer(&ud_obj, argv[1], val);
+		ud_set_mode(&ud_obj, 64);
+		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
+
+		while (ud_disassemble(&ud_obj))
+		{
+			printf("\t%s\n", ud_insn_hex(&ud_obj));
+		}
 	}
-	x = atoi(argv[1]);
-	if (x < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-	print_opcodes((char *)&main, x);
+
 	return (0);
 }
